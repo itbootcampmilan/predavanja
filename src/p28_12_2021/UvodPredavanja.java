5c55952fb31622dbce9cc92bb73ccac52a2122a8
Milan Jovanovic <it.bootcamp.drive@gmail.com>
2021-12-28 19:01:17 +0100

    Print number to 10

---
+++ src/p28_12_2021/UvodPredavanja.java
@@ -0,0 +1,12 @@
+package p28_12_2021;
+
+public class UvodPredavanja {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.print(i + ", ");
+		}
+	}
+
+}
